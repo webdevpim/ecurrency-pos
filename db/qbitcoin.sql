2400944410ece3cf0030a4fa9059c8e293e0b865
QBitcoin Developer 1 <qbitcoin.dev@gmail.com>
2021-05-09 10:41:14 +0300

    sql schema

---
+++ db/qbitcoin.sql
@@ -0,0 +1,44 @@
+
+CREATE TABLE block (
+  height int unsigned NOT NULL PRIMARY KEY,
+  hash binary(32) NOT NULL,
+  weight bigint unsigned NOT NULL,
+  prev_hash binary(32) DEFAULT NULL,
+  UNIQUE (hash)
+);
+
+CREATE TABLE transaction (
+  id int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  hash binary(32) NOT NULL,
+  block_height int unsigned NOT NULL,
+  UNIQUE (hash),
+  KEY (block_height),
+  FOREIGN KEY (block_height) REFERENCES block (height) ON DELETE CASCADE
+);
+
+CREATE TABLE tx_data (
+  id int unsigned NOT NULL PRIMARY KEY,
+  data blob NOT NULL,
+  FOREIGN KEY (id) REFERENCES transaction (id) ON DELETE CASCADE
+);
+
+-- Actually these are qbt addresses
+CREATE TABLE IF NOT EXISTS open_script (
+  id int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  data blob NOT NULL,
+  UNIQUE (data)
+);
+
+CREATE TABLE IF NOT EXISTS txo (
+  value     bigint unsigned NOT NULL,
+  num          int unsigned NOT NULL,
+  tx_in        int unsigned NOT NULL,
+  tx_out       int unsigned DEFAULT NULL,
+  open_script  int unsigned NOT NULL,
+  close_script blob DEFAULT NULL,
+  PRIMARY KEY (tx_in, num),
+  INDEX (tx_out),
+  FOREIGN KEY (tx_in)       REFERENCES transaction (id) ON DELETE CASCADE,
+  FOREIGN KEY (tx_out)      REFERENCES transaction (id) ON DELETE SET NULL,
+  FOREIGN KEY (open_script) REFERENCES open_script (id) ON DELETE RESTRICT
+);
