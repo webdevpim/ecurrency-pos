c4a03f210feca8d966361c521ee06a49cde1feae
QBitcoin Developer 1 <qbitcoin.dev@gmail.com>
2021-06-09 17:09:36 +0300

    Restructure Script.pm

--- lib/QBitcoin/Script.pm
+++ lib/QBitcoin/Script.pm
@@ -26,34 +26,6 @@ use constant {
     TRUE  => "\x01",
 };
 
-my @OP_CMD;
-
-sub opcode_to_cmd($) {
-    my ($opcode) = @_;
-    $opcode =~ s/^OP_//;
-    return "cmd_" . lc($opcode);
-}
-
-foreach my $opcode (keys %{&OPCODES}) {
-    if (my $func = __PACKAGE__->can(opcode_to_cmd($opcode))) {
-        $OP_CMD[OPCODES->{$opcode}] = $func;
-    }
-}
-foreach my $opcode (keys %{&OPCODES}) {
-    if (!__PACKAGE__->can(opcode_to_cmd($opcode))) {
-        $OP_CMD[OPCODES->{$opcode}] //= sub { unimplemented(OPCODES->{$opcode}, @_) };
-    }
-}
-foreach my $opcode (0x01 .. 0x4b) {
-    $OP_CMD[$opcode] = sub { cmd_pushdatan($opcode, @_) };
-}
-
-sub unimplemented($$) {
-    my ($opcode, $state) = @_;
-    Infof("Unimplemented opcode 0x%02x", $opcode);
-    return 0;
-}
-
 sub unpack_int($) {
     my ($data) = @_;
     defined($data) or return undef;
@@ -123,6 +95,80 @@ sub pack_int($) {
     }
 }
 
+my %INT_2_1 = (
+    add => sub { $a + $b },
+    sub => sub { $a - $b },
+);
+my %BIN_1_1 = (
+    hash160 => sub { hash160($a) },
+);
+my %PUSH_CONST = (
+    false     => "",
+    "1negate" => pack_int(-1),
+    map { $_ => pack_int($_) } 1 .. 16,
+);
+
+my @OP_CMD;
+
+sub opcode_to_cmd($) {
+    my ($opcode) = @_;
+    $opcode =~ s/^OP_//;
+    return lc($opcode);
+}
+
+foreach my $opcode (keys %{&OPCODES}) {
+    my $cmd = opcode_to_cmd($opcode);
+    if (my $func = __PACKAGE__->can("cmd_$cmd")) {
+        $OP_CMD[OPCODES->{$opcode}] = $func;
+    }
+    elsif ($INT_2_1{$cmd}) {
+        $OP_CMD[OPCODES->{$opcode}] = sub {
+            my ($state) = @_;
+            return if $state->[2]->[0]; # ifstack
+            my $stack = $state->[1];
+            @$stack >= 2 or return 0;
+            local $a = unpack_int(pop @$stack) // return 0;
+            local $b = unpack_int(pop @$stack) // return 0;
+            push @$stack, pack_int($INT_2_1{$cmd}->());
+            return undef;
+        };
+    }
+    elsif ($BIN_1_1{$cmd}) {
+        $OP_CMD[OPCODES->{$opcode}] = sub {
+            my ($state) = @_;
+            return if $state->[2]->[0]; # ifstack
+            my $stack = $state->[1];
+            @$stack >= 2 or return 0;
+            local $a = $stack->[-1];
+            $stack->[-1] = $BIN_1_1{$cmd}->();
+            return undef;
+        };
+    }
+    elsif (exists $PUSH_CONST{$cmd}) {
+        $OP_CMD[OPCODES->{$opcode}] = sub {
+            my ($state) = @_;
+            return if $state->[2]->[0]; # ifstack
+            push @{$state->[1]}, $PUSH_CONST{$cmd};
+            return undef;
+        };
+    }
+
+}
+foreach my $opcode (keys %{&OPCODES}) {
+    if (!__PACKAGE__->can(opcode_to_cmd($opcode))) {
+        $OP_CMD[OPCODES->{$opcode}] //= sub { unimplemented(OPCODES->{$opcode}, @_) };
+    }
+}
+foreach my $opcode (0x01 .. 0x4b) {
+    $OP_CMD[$opcode] = sub { cmd_pushdatan($opcode, @_) };
+}
+
+sub unimplemented($$) {
+    my ($opcode, $state) = @_;
+    Infof("Unimplemented opcode 0x%02x", $opcode);
+    return 0;
+}
+
 sub pushdata($) {
     my ($data) = @_;
     my $cmd;
@@ -166,26 +212,6 @@ sub cmd_dup($) {
     return undef;
 }
 
-sub cmd_add($) {
-    my ($state) = @_;
-    return if $state->[2]->[0]; # ifstack
-    my $stack = $state->[1];
-    my $int1 = unpack_int(pop @$stack) // return 0;
-    my $int2 = unpack_int(pop @$stack) // return 0;
-    push @$stack, pack_int($int1+$int2);
-    return undef;
-}
-
-sub cmd_sub($) {
-    my ($state) = @_;
-    return if $state->[2]->[0]; # ifstack
-    my $stack = $state->[1];
-    my $int1 = unpack_int(pop @$stack) // return 0;
-    my $int2 = unpack_int(pop @$stack) // return 0;
-    push @$stack, pack_int($int1-$int2);
-    return undef;
-}
-
 sub cmd_return($) {
     # return if $state->[2]->[0]; # ifstack -- "return" cannot be inside "if" condition
     return 0;
@@ -208,22 +234,6 @@ sub cmd_equal($) {
     return undef;
 }
 
-sub cmd_false($) {
-    my ($state) = @_;
-    return if $state->[2]->[0]; # ifstack
-    my $stack = $state->[1];
-    push @$stack, FALSE;
-    return undef;
-}
-
-sub cmd_1($) {
-    my ($state) = @_;
-    return if $state->[2]->[0]; # ifstack
-    my $stack = $state->[1];
-    push @$stack, TRUE;
-    return undef;
-}
-
 sub cmd_equalverify($) {
     my ($state) = @_;
     return if $state->[2]->[0]; # ifstack
@@ -232,15 +242,6 @@ sub cmd_equalverify($) {
     my $data1 = pop @$stack;
     my $data2 = pop @$stack;
     return $data1 eq $data2 ? undef : 0;
-}
-
-sub cmd_hash160($) {
-    my ($state) = @_;
-    return if $state->[2]->[0]; # ifstack
-    my $stack = $state->[1];
-    @$stack >= 1 or return 0;
-    $stack->[-1] = hash160($stack->[-1]);
-    return undef;
 }
 
 sub cmd_checksig($) {
