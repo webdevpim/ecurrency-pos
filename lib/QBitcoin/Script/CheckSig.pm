f2d64c2251e4c4302c6036cbd7b6c2128b1f2657
QBitcoin Developer 1 <qbitcoin.dev@gmail.com>
2021-06-23 18:21:39 +0300

    check_tx_signature()

--- lib/QBitcoin/Script/CheckSig.pm
+++ lib/QBitcoin/Script/CheckSig.pm
@@ -13,8 +13,13 @@ sub cmd_checksig($) {
     @$stack >= 2 or return 0;
     my $pubkey = pop @$stack;
     my $signature = pop @$stack;
-    push @$stack, check_sig($state->tx->sign_data, $signature, $pubkey) ? TRUE : FALSE;
+    push @$stack, check_tx_signature($pubkey, $signature, $state->tx, $state->input_num) ? TRUE : FALSE;
     return undef;
+}
+
+sub check_tx_signature {
+    my ($pubkey, $signature, $tx, $input_num) = @_;
+    return check_sig($tx->sign_data, $signature, $pubkey);
 }
 
 1;
