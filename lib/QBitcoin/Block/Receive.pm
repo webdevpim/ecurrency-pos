3d113a7f1515dcf11b1f58b75d4ee43c695d26a9
QBitcoin Developer 1 <qbitcoin.dev@gmail.com>
2021-09-12 22:27:30 +0300

    Fix increase reputation for source peer

--- lib/QBitcoin/Block/Receive.pm
+++ lib/QBitcoin/Block/Receive.pm
@@ -281,7 +281,7 @@ sub receive {
     if ($self->received_from && $self->self_weight) {
         $self->received_from->peer->add_reputation(blockchain_synced() ? 2 : 0.02);
         if ($self->rcvd && $self->rcvd ne $self->received_from->peer->ip) {
-            my $src_peer = QBitcoin::Peer->get_or_create(type_id => $self->type_id, ip => $self->rcvd);
+            my $src_peer = QBitcoin::Peer->get_or_create(type_id => PROTOCOL_QBITCOIN, ip => $self->rcvd);
             $src_peer->add_reputation(blockchain_synced() ? 1 : 0.01);
         }
     }
