d21f677399529cbb99bda5444cbee5f11657366e
QBitcoin Developer 1 <qbitcoin.dev@gmail.com>
2021-10-10 07:14:26 +0545

    QBitcoin::WarnWrapper

---
+++ lib/QBitcoin/WarnWrapper.pm
@@ -0,0 +1,19 @@
+package QBitcoin::WarnWrapper;
+use warnings;
+use strict;
+
+use QBitcoin::Log;
+
+$SIG{__WARN__} = \&sigwarn;
+
+sub sigwarn {
+    local $SIG{__WARN__} = 'DEFAULT';
+
+    # Always one arg
+    my $arg = shift;
+    my $mes = ref $arg ? "Unexpected warning" : $arg;
+    Err($mes);
+    warn $arg if ref $arg;
+}
+
+1;
